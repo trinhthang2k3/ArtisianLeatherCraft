d3a8e4fa90eb9de2b68218c14aa26acf056b15ff
trinhthang2k3 <162941783+trinhthang2k3@users.noreply.github.com>
2024-09-10 15:33:27 +0700

    Update validator.js

--- config/validator.js
+++ config/validator.js
@@ -1,13 +1,16 @@
 const { check, validationResult } = require("express-validator");
+const VerificationCode = require("../models/verificationCode");
+const errors = require("admin-bro-mongoose/src/utils/errors");
+
 
 const userSignUpValidationRules = () => {
   return [
     check("name", "Name is required").not().isEmpty(),
-    check("email", "Invalid email").not().isEmpty().isEmail(),
     check("password", "Please enter a password with 6 or more characters")
       .not()
       .isEmpty()
-      .isLength({ min: 6, max: 16},),
+      .isLength({ min: 6, max: 15},)
+      .matches(/[\W_]/, "i").withMessage("Password must contain at least one special character"),
   ];
 };
 
@@ -31,6 +34,34 @@ const userContactUsValidationRules = () => {
       .isLength({ min: 10 }),
   ];
 };
+
+const validateVerificationCode = async (req, res, next) => {
+  try {
+    const verificationCodeDoc = await VerificationCode.findOne({}).sort({ _id: -1 });
+    if (!verificationCodeDoc) {
+      req.flash("error", "Invalid verification code or email");
+      await VerificationCode.deleteCode(generatedCode);
+      return res.redirect("/user/signupmail"); // Or appropriate error page
+    }
+    const generatedCode = verificationCodeDoc.code;
+    const userCode = req.body.verificationCode;
+    console.log(generatedCode);
+    console.log(userCode);
+
+    if (generatedCode !== userCode) {
+      req.flash("error", "Invalid verification code"); // Flash an error message
+      await VerificationCode.deleteCode(generatedCode);
+      return res.redirect("/user/signupmail"); // Redirect to the signup mail page or appropriate error page
+    }
+    next(); // Proceed to the next middleware or route handler
+    await VerificationCode.deleteCode(generatedCode);
+  } catch (error) {
+    console.log('Error occurred while validating verification code:', error);
+    req.flash("error", "An error occurred"); // Flash an error message
+    return res.redirect("/user/signupmail"); // Redirect to the signup mail page or appropriate error page
+  }
+};
+
 
 const validateSignup = (req, res, next) => {
   const errors = validationResult(req);
@@ -79,4 +110,5 @@ module.exports = {
   validateSignup,
   validateSignin,
   validateContactUs,
+  validateVerificationCode
 };
